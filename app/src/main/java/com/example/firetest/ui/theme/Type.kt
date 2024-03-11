eb81a23791663592c8a36fe86f11a2994e820a01
Hibro-onyancha <hibroonyancha9@gmail.com>
2024-03-11 21:52:40 +0300

    v1.1

--- app/src/main/java/com/example/firetest/ui/theme/Type.kt
+++ app/src/main/java/com/example/firetest/ui/theme/Type.kt
@@ -1,10 +1,31 @@
 package com.example.firetest.ui.theme
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+/*this will describe all t he type of shapes that will be held in amy composable*/
+val smallShape = RoundedCornerShape(SMALL_SHAPE.dp)
+val largeShape = RoundedCornerShape(LARGE_SHAPE.dp)
+val mediumShape = RoundedCornerShape(MEDIUM_SHAPE.dp)
+
+/*this one will describe the common heights  that we will use through this project*/
+val standardHeight = MEDIUM_HEIGHT.dp
+
+/**for icons*/
+val iconStandard = ICON_STANDARD.dp
+val iconLarge = ICON_LARGE.dp
+
+/*this will describe all text sizes that will be used in the entire project*/
+val smallText = SMALL_TEXT.sp
+val largeText = LARGE_TEXT.sp
+val extraLargeText = EXTRA_LARGE_TEXT.sp
+val mediumText = MEDIUM_SMALL_TEXT.sp
+val mediumLargeText = MEDIUM_LARGE_TEXT.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
---
+++ app/src/main/java/com/example/firetest/ui/theme/values.kt
@@ -0,0 +1,17 @@
+package com.example.firetest.ui.theme
+
+const val LARGE_SHAPE = 20
+const val MEDIUM_SHAPE = 15
+const val SMALL_SHAPE = 10
+
+/*constant height*/
+const val MEDIUM_HEIGHT = 60
+/*icon sizes*/
+const val ICON_STANDARD= 24
+const val ICON_LARGE = 34
+/*values for text size*/
+const val EXTRA_LARGE_TEXT = 27
+const val LARGE_TEXT = 22
+const val MEDIUM_LARGE_TEXT = 17
+const val MEDIUM_SMALL_TEXT = 15
+const val SMALL_TEXT = 10
